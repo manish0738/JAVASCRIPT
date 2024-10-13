07b256d34c3774c65d1706b4544802d84515c769
MANISH PATIL <manishpatil0738@outlook.com>
2024-10-13 16:14:49 +0000

    datatype

---
+++ DAY3/DATATYPES.js
@@ -0,0 +1,36 @@
+"use strict" //treat all JS code as newer version
+
+// alert( 3 + 3 )
+
+// console.log("Manish");
+
+let name = "Manish"
+let age = 19
+let isLoggedIn = true
+
+let exam = null
+let state;
+
+// number => 2 to power 53
+// Bigint
+// string
+// boolean => true of false
+// null => standalone value 
+// undefined
+// symbol 
+
+
+
+//objects
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state); 
+console.log(typeof exam); 
+
+
+// we use state as null then type of show object but we do not assign value then show undefined 
+
+
+
