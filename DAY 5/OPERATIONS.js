0d42b461da391c6d33d84591d0c381b55703ebd3
MANISH PATIL <manishpatil0738@outlook.com>
2024-10-16 15:34:39 +0000

    OPERATIONS

--- DAY 5/OPERATIONS.js
+++ DAY 5/OPERATIONS.js
@@ -15,5 +15,22 @@ console.log( 1 + "2" );
 console.log( "1" + 2 + 2 );
 console.log( 1 + 2 + "2" );
 
+console.log(true);
+// DISPLAY true
+console.log(+true);
+// DISPLAY 1
+console.log(+"");
+// DISPLAY 0
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+// WE USE PREFIX OR SUFFIX INCREMENT OR DECREMENT
+gameCounter++;
+console.log(gameCounter);
+
 
 
