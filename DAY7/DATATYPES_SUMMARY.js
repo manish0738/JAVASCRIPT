7aa50939aa7c582412cf1c7089bbe6fa06998b17
MANISH PATIL <manishpatil0738@outlook.com>
2024-10-18 17:02:27 +0000

    SUMMARY

---
+++ DAY7/DATATYPES_SUMMARY.js
@@ -0,0 +1,30 @@
+// PRIMITIVE DATATYPES 
+
+// 7 TYPES OF PRIMITIVE DATATYPES
+
+// STRING, NUMBER, BOOLEAN, NULL, UNDEFINED, SYMBOL, BIGINT
+
+let name = "MANISH"
+let score = 197
+let scoreDecimal = 197.197
+let toss = true
+let age = null
+let salary;
+let class = symbol("BCA")
+let classNew = symbol("BCA")
+
+
+// NOT-PRIMITIVE DATATYPES (REFERENCE)
+// ARRAY, OBJECT, FUNCTION
+
+let heros = ["iron-man", "spiderman", "captain-america"]
+let myObj{
+    name = "MANISH"
+    age = 19
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof name);
